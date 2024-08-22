98b5e71ae8b820af9692fc0cfb45c896d6e89bdf
Meharsahni07 <meharsahni2000@gmail.com>
2024-08-22 20:27:54 +0530

    Updated

--- admin/home.php
+++ admin/home.php
@@ -1,12 +1,14 @@
+<?php #print_r($resp);die; ?>
 <h1 class="">Welcome to Stock Management System</h1>
 <hr>
 <div class="row">
+    
     <div class="col-12 col-sm-6 col-md-3">
         <div class="info-box bg-light shadow">
             <span class="info-box-icon bg-info elevation-1"><i class="fas fa-th-list"></i></span>
 
             <div class="info-box-content">
-            <span class="info-box-text">PO Records</span>
+            <span class="info-box-text">PO Records </span>
             <span class="info-box-number text-right">
                 <?php 
                     echo $conn->query("SELECT * FROM `purchase_order_list`")->num_rows;
@@ -133,3 +135,39 @@
     </div>
     <?php endif; ?>
 </div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Developers</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <div class="row">
+            <div class='col-sm-6'>
+              <img src="<?php echo base_url.'uploads/developer.jpg' ?>" style="width:200px;" alt="User Image">
+            </div>
+            <div class='col-sm-6'>
+              <h4>Mehar Sahani</h4>
+              <h6>+91 8766293800</h6>
+              <h6>sahnimehar@gmail.com</h6>
+              <h6>Manipal University, Jaipur</h6>
+            </div>
+              
+          </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<script>
+    $(document).ready(function(){
+        // Trigger modal on page load
+        $('#exampleModal').modal('show');
+    });
+</script>
+
